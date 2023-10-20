d8b44727e7c2d83c06add0d824e85fde9d0a7bfd
Ayse Konya <konyaayse@gmail.com>
2023-10-20 14:45:20 +0100

    homepage initial setting

--- src/test/java/stepDefinitions/UI_StepDef/homePage/HP.java
+++ src/test/java/stepDefinitions/UI_StepDef/homePage/HP.java
@@ -1,4 +1,19 @@
 package stepDefinitions.UI_StepDef.homePage;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 public class HP {
+
+    @Given("user goes to homepage")
+    public void user_goes_to_homepage() {
+
+
+    }
+    @Then("user verifies social media icons are visible")
+    public void user_verifies_social_media_icons_are_visible() {
+
+
+    }
+
 }
---
+++ src/test/resources/features/UI_features/homePage/aysKnyHomepage.feature
@@ -0,0 +1,6 @@
+Feature: homepage
+  @US_003
+  @UI
+  Scenario: Verify social media icons are visible on the homepage
+    Given user goes to homepage
+    Then user verifies social media icons are visible
