253dc004a981b1ea2d7f44fc8e3d441fba85491e
EsraBaldiz <esracagan85@gmail.com>
2023-10-21 17:48:21 +0200

    09featurefile

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,6 +4,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends CommonPage{
+
     @FindBy(xpath="//*[@data-test-id='loginScreen_logIn']")
     public WebElement loginButton;
+
+    @FindBy(xpath = "//div[text()='Product Demo']")//us09
+    public WebElement productDemo;
+    @FindBy(xpath = "//div[text()='How Does Hypnotes Work?']")
+    public WebElement hynotesWork;
+    @FindBy(xpath = "//div[text()='Pricing']")
+    public WebElement pricing;
+    @FindBy(xpath = "//div[text()='HIPAA Compliance']")
+    public WebElement hipaaCompliance;
+
+    @FindBy(xpath = "//div[text()='Secure Login, Account Verification & Data Storage']")
+    public WebElement secureLogin;
+
+    @FindBy(xpath = "//div[text()='Reviews/Testimonials']")
+    public WebElement reviewsTestimonials;
+    @FindBy(xpath = "//div[text()='Get Started For Free']")
+    public WebElement getStarted;//us09
+
 }
---
+++ src/test/java/stepDefinitions/UI_StepDef/homePage/US_09HomePageOurPlatformStepDef.java
@@ -0,0 +1,100 @@
+package stepDefinitions.UI_StepDef.homePage;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US_09HomePageOurPlatformStepDef {
+    @Given("User goes to home page")
+    public void userGoesToHomePage() {
+
+    }
+
+    @When("The user sees our platform section at the bottom of the page")
+    public void theUserSeesOurPlatformSectionAtTheBottomOfThePage() {
+
+    }
+
+    @Then("The user verifies that the product demo button is visible and clickable")
+    public void theUserVerifiesThatTheProductDemoButtonIsVisibleAndClickable() {
+
+    }
+
+    @And("The user clicks on the product demo button, the user sees the relevant page.")
+    public void theUserClicksOnTheProductDemoButtonTheUserSeesTheRelevantPage() {
+
+    }
+
+    @Then("The user verifies that the How Does Hypnotes Work? button is visible and clickable")
+    public void theUserVerifiesThatTheHowDoesHypnotesWorkButtonIsVisibleAndClickable() {
+
+    }
+
+    @And("The user clicks on the product How Does Hypnotes Work?, the user sees the relevant page.")
+    public void theUserClicksOnTheProductHowDoesHypnotesWorkTheUserSeesTheRelevantPage() {
+
+    }
+
+    @Then("The user verifies that the Pricing button is visible and clickable")
+    public void theUserVerifiesThatThePricingButtonIsVisibleAndClickable() {
+
+    }
+
+    @And("The user clicks on the product Pricing, the user sees the relevant page.")
+    public void theUserClicksOnTheProductPricingTheUserSeesTheRelevantPage() {
+
+    }
+
+    @Then("The user verifies that the HIPAA Compliance button is visible and clickable")
+    public void theUserVerifiesThatTheHIPAAComplianceButtonIsVisibleAndClickable() {
+
+    }
+
+    @And("The user clicks on the product HIPAA Compliance, the user sees the relevant page.")
+    public void theUserClicksOnTheProductHIPAAComplianceTheUserSeesTheRelevantPage() {
+
+    }
+
+    @Then("The user verifies that the Secure Login, Account Verification & Data Storage button is visible and clickable")
+    public void theUserVerifiesThatTheSecureLoginAccountVerificationDataStorageButtonIsVisibleAndClickable() {
+
+    }
+
+    @And("The user clicks on the product Secure Login, Account Verification & Data Storage, the user sees the relevant page.")
+    public void theUserClicksOnTheProductSecureLoginAccountVerificationDataStorageTheUserSeesTheRelevantPage() {
+
+    }
+
+
+
+    @Then("The user verifies that the Get Started For Free button is visible and clickable")
+    public void theUserVerifiesThatTheGetStartedForFreeButtonIsVisibleAndClickable() {
+
+    }
+
+    @And("The user clicks on the Get Started For Free, the user sees the relevant page.")
+    public void theUserClicksOnTheGetStartedForFreeTheUserSeesTheRelevantPage() {
+
+    }
+
+
+    @Then("The user verifies that the Reviews\\/Testimonials button is visible and clickable{int}")
+    public void theUserVerifiesThatTheReviewsTestimonialsButtonIsVisibleAndClickable(int arg0) {
+
+    }
+
+    @And("The user clicks on the Reviews\\/Testimonials, the user sees the relevant page{int}")
+    public void theUserClicksOnTheReviewsTestimonialsTheUserSeesTheRelevantPage(int arg0) {
+
+    }
+
+    @Then("The user verifies that the Reviews Testimonials button is visible and clickable")
+    public void theUserVerifiesThatTheReviewsTestimonialsButtonIsVisibleAndClickable() {
+
+    }
+
+    @And("The user clicks on the Reviews Testimonials, the user sees the relevant page")
+    public void theUserClicksOnTheReviewsTestimonialsTheUserSeesTheRelevantPage() {
+    }
+}
