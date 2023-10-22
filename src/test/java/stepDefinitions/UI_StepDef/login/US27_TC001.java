7cd284191f1884ffe10a74cdd3bbfaa70e9d5d8a
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-10-22 16:31:09 +0200

    22 oct

---
+++ src/test/java/stepDefinitions/UI_StepDef/login/US27_TC001.java
@@ -0,0 +1,30 @@
+package stepDefinitions.UI_StepDef.login;
+
+import io.cucumber.java.en.*;
+import pages.CommonPage;
+
+import static stepDefinitions.Hooks.commonPage;
+
+public class US27_TC001 {
+
+    @When("clicks the login button")
+    public void clicks_the_login_button() {
+
+    }
+    @When("clicks to Sign in with apple")
+    public void clicks_to_sign_in_with_apple() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("enters the e-mail address and password")
+    public void enters_the_e_mail_address_and_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("assert that the user is logged in")
+    public void assert_that_the_user_is_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
