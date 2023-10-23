e07e7657abd3a4cb72edc5503a8c3491460d85f0
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-10-23 17:56:35 +0200

    23 oct

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,26 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage extends CommonPage{
+    @FindBy(xpath = "//span[@data-test-id='loginScreen_logIn']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//div[@class='LoginForm_fontSmall__y0l1R']")
+    public WebElement loginWithApple;
+
+    @FindBy(xpath = "//div[@class='LoginForm_fontSmall__y0l1R']")
+    public WebElement loginWithLinkedIn;
+
+    @FindBy(xpath = "//input[@id='account_name_text_field']")
+    public WebElement emailApple;
+
+    @FindBy(xpath = "//input[@id='password_text_field']")
+    public WebElement passwordApple;
+
+    @FindBy(xpath = "//a[@class='ant-dropdown-trigger']")
+    public WebElement profile;
+    //login olmayi assert etmek icin
 
 }
--- src/test/java/stepDefinitions/UI_StepDef/login/US27_TC001.java
+++ src/test/java/stepDefinitions/UI_StepDef/login/US27_TC001.java
@@ -1,30 +1,38 @@
 package stepDefinitions.UI_StepDef.login;
 
 import io.cucumber.java.en.*;
-import pages.CommonPage;
+import pages.LoginPage;
+import utilities.ReusableMethods;
 
 import static stepDefinitions.Hooks.commonPage;
 
 public class US27_TC001 {
 
-    @When("clicks the login button")
-    public void clicks_the_login_button() {
+    LoginPage loginPage = new LoginPage();
 
+    @When("clicks the login button")
+    public void clicks_the_login_button() throws InterruptedException {
+        loginPage.loginButton.click();
+        Thread.sleep(2000);
     }
+
     @When("clicks to Sign in with apple")
-    public void clicks_to_sign_in_with_apple() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void clicks_to_sign_in_with_apple()  {
+        loginPage.loginWithApple.click();
+        ReusableMethods.waitFor(2000);
+
     }
     @When("enters the e-mail address and password")
     public void enters_the_e_mail_address_and_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        ReusableMethods.waitFor(2000);
+        loginPage.emailApple.sendKeys("kubrakurdoglusimsek@gmail.com");
+        ReusableMethods.waitFor(2000);
+        loginPage.passwordApple.sendKeys("Kubra");
+
     }
     @Then("assert that the user is logged in")
     public void assert_that_the_user_is_logged_in() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 }
