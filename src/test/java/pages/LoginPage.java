b3adc80d6fa4eb1f64d843f3b41b592e242ade28
fatmacekimguler <fatmacekimguler@gmail.com>
2023-10-24 21:05:39 +0100

    US_034 is starting

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,13 @@
 package pages;
 
-public class LoginPage extends CommonPage{
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+public class LoginPage extends CommonPage{
+    @FindBy(xpath = "//input[@name='username']")
+    public WebElement email_login;
+    @FindBy(xpath = "//input[@name='password']")
+    public WebElement password_Login;
+    @FindBy(xpath = "//button[@class='ant-btn css-15rg2km ant-btn-default LoginForm_loginBtn__1yNyd']")
+    public WebElement login_button;
 }
