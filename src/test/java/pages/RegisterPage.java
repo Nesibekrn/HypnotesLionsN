5401adf1ab78705ff111bfad26931f8041d64928
mervecakir <m.cakir88w@gmail.com>
2023-10-24 14:24:14 +0100

    UPDATE

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -6,16 +6,22 @@ import org.openqa.selenium.support.FindBy;
 public class RegisterPage extends CommonPage{
     @FindBy(xpath="//span[contains(text(),'Sign Up for Free')]")
     public WebElement signUpForFree;
+
     @FindBy(css = "div.register_wrapper___MAvD div.register_row__XC4up div.register_col__zTnRz div.RegisterForm_formContanier__TUs7y div.RegisterForm_form__n23C0 div.RegisterForm_formGroup__03WXK:nth-child(1) div.RegisterForm_scoialIconContanier__gaFVJ a:nth-child(1) > div.RegisterForm_socRowCont__fpaxX")
     public WebElement signWithGoogle;
+
     @FindBy(xpath="//div[@data-test-id='registerAs_a_clientPage_signInWithFacebook']")
     public WebElement signWithFacebook;
+
     @FindBy(linkText = "I am a Client")
     public WebElement clientLogin;
+
     @FindBy(xpath="//span[contains(text(),\"Next\")]")
     public WebElement nextButton;
+
     @FindBy(xpath="//input[@id=\"identifierId\"]")
     public WebElement  emailOrPhone;
+
     @FindBy(xpath="//input[@name=\"Passwd\"]")
     public WebElement  passwordGoogle;
 
