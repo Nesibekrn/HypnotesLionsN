740c13e86e59e9338a15e10a80b798f91c3895f9
EsraBaldiz <esracagan85@gmail.com>
2023-10-24 18:08:50 +0200

    us09ASSERT

--- src/test/java/stepDefinitions/UI_StepDef/homePage/US_09HomePageOurPlatformStepDef.java
+++ src/test/java/stepDefinitions/UI_StepDef/homePage/US_09HomePageOurPlatformStepDef.java
@@ -17,8 +17,8 @@ import utilities.ReusableMethods;
 import static org.junit.Assert.assertTrue;
 import static stepDefinitions.Hooks.driver;
 
-public class US_09HomePageOurPlatformStepDef  {
-    HomePage homePage=new HomePage();
+public class US_09HomePageOurPlatformStepDef {
+    HomePage homePage = new HomePage();
 
     @Given("User goes to home page")
     public void userGoesToHomePage() {
@@ -28,32 +28,27 @@ public class US_09HomePageOurPlatformStepDef  {
 
     @When("The user sees our platform section at the button of the page")
     public void theUserSeesOurPlatformSectionAtTheBottomOfThePage() {
-  JS_utilities.scrollDownByJS();
-  ReusableMethods.waitFor(2);
-      JavascriptExecutor js = (JavascriptExecutor) driver;
-      js.executeScript("window.scrollBy(0, 500)");
- Assert.assertTrue(homePage.ourPlatform.isDisplayed());
-
+        JS_utilities.scrollDownByJS();
+        ReusableMethods.waitFor(2);
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0, 500)");
+        Assert.assertTrue(homePage.ourPlatform.isDisplayed());
 
 
     }
 
     @Then("The user verifies that the product demo button is visible and clickable")
     public void theUserVerifiesThatTheProductDemoButtonIsVisibleAndClickable() {
-     Assert.assertTrue(homePage.productDemo.isDisplayed());
-
-      Assert.assertTrue(homePage.productDemo.isEnabled());
-      ReusableMethods.waitFor(3);
-    }
-
-    private void assertTrue(boolean enabled, boolean b) {
-        Assert.assertTrue(homePage.productDemo.isEnabled());
+        Assert.assertTrue(homePage.productDemo.isDisplayed());
+        ReusableMethods.waitFor(2);
+        homePage.productDemo.click();
         ReusableMethods.waitFor(3);
     }
 
+
     @And("The user clicks on the product demo button, the user sees the relevant page.")
     public void theUserClicksOnTheProductDemoButtonTheUserSeesTheRelevantPage() {
-       // WebElement openedElement =homePage.productDemo;
+        // WebElement openedElement =homePage.productDemo;
         boolean isOpened = homePage.productDemo.isDisplayed();
         if (isOpened) {
             System.out.println("Sayfa açıldı.");
@@ -66,9 +61,9 @@ public class US_09HomePageOurPlatformStepDef  {
     @Then("The user verifies that the How Does Hypnotes Work? button is visible and clickable")
     public void theUserVerifiesThatTheHowDoesHypnotesWorkButtonIsVisibleAndClickable() {
         //Assert.assertTrue(String.valueOf(homePage.hynotesWork.isDisplayed()),false);
-        assertTrue(Boolean.parseBoolean(String.valueOf(homePage.hynotesWork.isDisplayed())),false);
-        ReusableMethods.waitFor(3);
-        assertTrue(homePage.hynotesWork.isEnabled(), false);
+        Assert.assertTrue(homePage.hynotesWork.isDisplayed());
+        ReusableMethods.waitFor(2);
+        homePage.hynotesWork.click();
         ReusableMethods.waitFor(3);
 
     }
@@ -87,9 +82,9 @@ public class US_09HomePageOurPlatformStepDef  {
 
     @Then("The user verifies that the Pricing button is visible and clickable")
     public void theUserVerifiesThatThePricingButtonIsVisibleAndClickable() {
-      assertTrue(Boolean.parseBoolean(String.valueOf(homePage.pricing.isDisplayed())),false);
-        ReusableMethods.waitFor(3);
-        assertTrue(homePage.pricing.isEnabled(), false);
+        assertTrue(homePage.pricing.isDisplayed());
+        ReusableMethods.waitFor(2);
+        homePage.pricing.click();
         ReusableMethods.waitFor(3);
     }
 
@@ -106,9 +101,9 @@ public class US_09HomePageOurPlatformStepDef  {
 
     @Then("The user verifies that the HIPAA Compliance button is visible and clickable")
     public void theUserVerifiesThatTheHIPAAComplianceButtonIsVisibleAndClickable() {
-    assertTrue(Boolean.parseBoolean(String.valueOf(homePage.hipaaCompliance.isDisplayed())),false);
-        ReusableMethods.waitFor(3);
-        assertTrue(homePage.hipaaCompliance.isEnabled(), false);
+        assertTrue(homePage.hipaaCompliance.isDisplayed());
+        ReusableMethods.waitFor(2);
+       homePage.hipaaCompliance.click();
         ReusableMethods.waitFor(2);
     }
 
@@ -125,9 +120,9 @@ public class US_09HomePageOurPlatformStepDef  {
 
     @Then("The user verifies that the Secure Login, Account Verification & Data Storage button is visible and clickable")
     public void theUserVerifiesThatTheSecureLoginAccountVerificationDataStorageButtonIsVisibleAndClickable() {
-      assertTrue(Boolean.parseBoolean(String.valueOf(homePage.secureLogin.isDisplayed())),false);
-        ReusableMethods.waitFor(3);
-        assertTrue(homePage.secureLogin.isEnabled(), false);
+        assertTrue(homePage.secureLogin.isDisplayed());
+        ReusableMethods.waitFor(2);
+        homePage.secureLogin.click();
         ReusableMethods.waitFor(2);
     }
 
@@ -139,16 +134,15 @@ public class US_09HomePageOurPlatformStepDef  {
         } else {
             System.out.println("Sayfa açılmadı.");
         }
-        ReusableMethods.waitFor(3);
+        ReusableMethods.waitFor(2);
     }
-
 
 
     @Then("The user verifies that the Get Started For Free button is visible and clickable")
     public void theUserVerifiesThatTheGetStartedForFreeButtonIsVisibleAndClickable() {
-       assertTrue(Boolean.parseBoolean(String.valueOf(homePage.getStarted.isDisplayed())),false);
-        ReusableMethods.waitFor(3);
-        assertTrue(homePage.getStarted.isEnabled(), false);
+       assertTrue(homePage.getStarted.isDisplayed());
+        ReusableMethods.waitFor(2);
+     homePage.getStarted.click();
         ReusableMethods.waitFor(2);
     }
 
@@ -160,15 +154,15 @@ public class US_09HomePageOurPlatformStepDef  {
         } else {
             System.out.println("Sayfa açılmadı.");
         }
-        ReusableMethods.waitFor(3);
+        ReusableMethods.waitFor(2);
     }
 
     @Then("The user verifies that the Reviews Testimonials button is visible and clickable")
     public void theUserVerifiesThatTheReviewsTestimonialsButtonIsVisibleAndClickable() {
-    assertTrue(Boolean.parseBoolean(String.valueOf(homePage.reviewsTestimonials.isDisplayed())),false);
-        ReusableMethods.waitFor(3);
-        assertTrue(homePage.reviewsTestimonials.isEnabled(), false);
-        ReusableMethods.waitFor(3);
+        assertTrue(homePage.reviewsTestimonials.isDisplayed());
+        ReusableMethods.waitFor(2);
+       homePage.reviewsTestimonials.click();
+        ReusableMethods.waitFor(2);
     }
 
     @And("The user clicks on the Reviews Testimonials, the user sees the relevant page")
