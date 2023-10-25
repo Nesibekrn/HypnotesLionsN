f7274274e5e4d39e39c1cc882a6d403e937cb13e
Nesibe Karan <nesibekrn@gmail.com>
2023-10-25 14:28:27 +0300

    dublication issue

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -46,32 +46,7 @@ public class LoginPage extends CommonPage{
     public WebElement needAnAccount_link;
     @FindBy(xpath = "//span[@class='ant-btn-icon']")
     public WebElement showPassword_icon;
-    @FindBy(xpath = "//span[@data-test-id='loginScreen_logIn']")
-    public WebElement loginButton;
 
-    @FindBy(xpath = "(//div[@class='LoginForm_fontSmall__y0l1R'])[4]")
-    public WebElement loginWithApple;
 
-    @FindBy(xpath = "(//div[@class='LoginForm_fontSmall__y0l1R'])[3]")
-    public WebElement loginWithLinkedIn;
-
-    @FindBy(xpath = "//input[@type='text']")
-    public WebElement emailApple;
-
-    @FindBy(xpath = "//input[@id='password_text_field']")
-    public WebElement passwordApple;
-
-    @FindBy(xpath = "//a[@class='ant-dropdown-trigger']")
-    public WebElement profile;
-    //login olmayi assert etmek icin
-
-    @FindBy(xpath = "//button[@id='sign-in']")
-    public WebElement emailSend;
-
-    @FindBy(xpath = "//div[@class='overflow-text']")
-    public WebElement continuerButton;
-
-    @FindBy(xpath = "//button[@id='unlock-account-1698098954911-9']")
-    public WebElement alertMessage;
 
 }
