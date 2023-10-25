5686be2e22c133ea82017c81253d556a35ec0bc6
mervecakir <m.cakir88w@gmail.com>
2023-10-25 18:46:14 +0100

    US16 IS DONE

--- src/test/java/stepDefinitions/UI_StepDef/register/US_016_ClientRgstr_Facebook.java
+++ src/test/java/stepDefinitions/UI_StepDef/register/US_016_ClientRgstr_Facebook.java
@@ -5,7 +5,6 @@ import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import pages.CommonPage;
 import utilities.ReusableMethods;
-
 import static stepDefinitions.Hooks.driver;
 
 public class US_016_ClientRgstr_Facebook extends CommonPage {
@@ -27,7 +26,6 @@ public class US_016_ClientRgstr_Facebook extends CommonPage {
     public void the_user_verifies_sign_in_with_facebook_button()throws InterruptedException {
         getRegisterPage().signWithFacebook.click();
         ReusableMethods.waitFor(2);
-
     }
     @Then("the user verifies related page")
     public void the_user_verifies_related_page() {
