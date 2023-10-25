48ca81f451eb61e3cb84791ab33ca2209c17c9fe
mervecakir <m.cakir88w@gmail.com>
2023-10-25 19:37:55 +0100

    update

--- src/test/java/pages/RegisterPage.java
+++ src/test/java/pages/RegisterPage.java
@@ -7,8 +7,10 @@ public class RegisterPage extends CommonPage{
     @FindBy(xpath="//span[contains(text(),'Sign Up for Free')]")
     public WebElement signUpForFree;
 
+
     @FindBy(css = "div.register_wrapper___MAvD div.register_row__XC4up div.register_col__zTnRz div.RegisterForm_formContanier__TUs7y div.RegisterForm_form__n23C0 div.RegisterForm_formGroup__03WXK:nth-child(1) div.RegisterForm_scoialIconContanier__gaFVJ a:nth-child(1) > div.RegisterForm_socRowCont__fpaxX")
     public WebElement signWithGoogle;
+
 
     @FindBy(xpath="//div[@data-test-id='registerAs_a_clientPage_signInWithFacebook']")
     public WebElement signWithFacebook;
@@ -77,5 +79,5 @@ public class RegisterPage extends CommonPage{
     @FindBy(xpath = "//p[normalize-space()='A special character']")
     public WebElement specialCharacterValidation;
 
-
+// conflict
 }
