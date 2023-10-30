d95c1f098822d0086f534b5ff4c12c65b67d4d0e
SAMET ARI <sametari@SAMET-MBP.lan>
2023-10-30 16:07:58 +0100

    us31

--- src/test/java/US_031_Stepdefs.java
+++ src/test/java/US_031_Stepdefs.java
@@ -8,24 +8,26 @@ import pages.RegisterPage;
 
 public class US_031_Stepdefs extends CommonPage {
 
+
     @Then("Verify that the color of messages for {string} and {string} and {string} and {string} and {string}")
-    public void verifyThatTheColorOfMessagesForAndAndAndAndAnd(String Chars, String lower, String upper, String numb, String special) {
+    public void verify_that_the_color_of_messages_for_and_and_and_and(String Chars, String lower, String upper, String numb, String special) {
 
         String pass = Chars+lower+upper+numb+special;
         getRegisterPage().input_password.clear();
 
-        String greyColor = "rgba(255, 0, 0, 1)";
+
+        String redColor = "rgba(255, 0, 0, 1)";
         String greenColor = "rgba(35, 108, 47, 1)";
         String actualColor;
 
         actualColor = getRegisterPage().numberValidation.getCssValue("color");
-        Assert.assertEquals(greyColor, actualColor);
+        Assert.assertEquals(redColor, actualColor);
 
         getRegisterPage().input_password.sendKeys(pass);
 
         if (pass.length()<8 | pass.length()>50){
             actualColor = getRegisterPage().characterValidation.getCssValue("color");
-            Assert.assertEquals(greyColor, actualColor);
+            Assert.assertEquals(redColor, actualColor);
         } else {
             actualColor = getRegisterPage().characterValidation.getCssValue("color");
             Assert.assertEquals(greenColor, actualColor);
@@ -33,7 +35,7 @@ public class US_031_Stepdefs extends CommonPage {
 
         if (lower.isEmpty()) {
             actualColor = getRegisterPage().lowercaseLetterValidation.getCssValue("color");
-            Assert.assertEquals(greyColor, actualColor);
+            Assert.assertEquals(redColor, actualColor);
         } else {
             actualColor = getRegisterPage().lowercaseLetterValidation.getCssValue("color");
             Assert.assertEquals(greenColor, actualColor);
@@ -41,7 +43,7 @@ public class US_031_Stepdefs extends CommonPage {
 
         if (upper.isEmpty()) {
             actualColor = getRegisterPage().uppercaseValidation.getCssValue("color");
-            Assert.assertEquals(greyColor, actualColor);
+            Assert.assertEquals(redColor, actualColor);
         } else {
             actualColor= getRegisterPage().uppercaseValidation.getCssValue("color");
             Assert.assertEquals(greenColor, actualColor);
@@ -52,7 +54,7 @@ public class US_031_Stepdefs extends CommonPage {
 
         if (special.isEmpty()) {
             actualColor = getRegisterPage().specialCharacterValidation.getCssValue("color");
-            Assert.assertEquals(greyColor, actualColor);
+            Assert.assertEquals(redColor, actualColor);
         } else {
             actualColor = getRegisterPage().specialCharacterValidation.getCssValue("color");
             Assert.assertEquals(greenColor, actualColor);
@@ -61,14 +63,11 @@ public class US_031_Stepdefs extends CommonPage {
     }
 
     @Then("Verify that the sign up button for Password is clickable for only {string} is True")
-    public void verifyThatTheSignUpButtonForIsClickableForPasswordOnlyIsTrue(String isVal) {
-
+    public void verify_that_the_sign_up_button_for_password_is_clickable_for_only_is_true(String isVal) {
         if (isVal.equals("F")) {
             Assert.assertFalse("Sign Up button is not clickable", getRegisterPage().button_signUp.isEnabled());
         } else {
             Assert.assertTrue("Sign Up button is clickable", getRegisterPage().button_signUp.isEnabled());
         }
+    }}
 
-    }
-
-    }
--- src/test/resources/features/UI_features/register/US_031.feature
+++ src/test/resources/features/UI_features/register/US_031.feature
@@ -3,10 +3,11 @@
 #	It should not be possible to register with invalid passwords.
 #	Password must contain the following: 8-50 characters, A lowercase letter, A capital (uppercase) letter, A number, A special character
 Feature: As a user, I should be able to check valid and invalid passwords
-
+  @UI
+  @US31
   Scenario Outline: TC_001 Appropriate expressions entered in the Password section should change the color (red to green)
     Given user goes to therapist register page
-    Then user clicks on Sign Up button
+    #Then user clicks on Sign Up button
     When user clicks password field
     Then Verify that the color of messages for "<Chars>" and "<LowerCase>" and "<UpperCase>" and "<Number>" and "<Special>"
     Then Verify that the sign up button for Password is clickable for only "<Val>" is True
