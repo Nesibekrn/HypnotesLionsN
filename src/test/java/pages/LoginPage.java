c236eb15bda80bd9042d5fe8109ffffb00147fe2
zeynepsteel <lilyfoster1987@gmail.com>
2023-11-02 12:49:42 -0400

    us59

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -5,7 +5,124 @@ import org.openqa.selenium.support.FindBy;
 
 import static stepDefinitions.Hooks.driver;
 
-public class LoginPage extends CommonPage{
+import org.openqa.selenium.support.PageFactory;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+public class LoginPage extends CommonPage {
+
+    public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);//bu sayfaya gel driver'da bu locate i bul page factory sayesinde
+    }
+    //fatih
+    @FindBy(xpath = "//span[@class='ant-typography style_signUpBtn__JTRJm css-15rg2km']")
+    public WebElement SignUpForFree;
+    @FindBy(xpath = "//input[@data-test-id=' registerPage_fullname']")
+    public WebElement FullNameButtonForSignUpForFree;
+    @FindBy(xpath = "//input[@data-test-id=' registerPage_email']")
+    public WebElement EmailButtonForSignUpForFree;
+    @FindBy(xpath = "//input[@data-test-id=' registerPage_password']")
+    public WebElement PasswordButtonForSignUpForFree;
+    @FindBy(xpath = "//button[@data-test-id='registerPage_signup_button']")
+    public WebElement SignUpButtonForSignUpForFree;
+    @FindBy(xpath = "//a[contains(text(),'VERIFY EMAIL')]")
+    public WebElement ButtonVERIFYeMAIL;
+
+
+    @FindBy(xpath = "//span[@class='ant-typography style_logInBtn__PV10a css-15rg2km' and @data-test-id='loginScreen_logIn']")
+    public WebElement Login;
+
+
+    @FindBy(xpath = "//a[@href='/reset-password']")
+    public WebElement ForgotYourPassword;
+
+    @FindBy(xpath = "//h3")
+    public WebElement ForgotYourPasswordWindow;
+
+    @FindBy(xpath = "//input[@data-test-id='Forgot_password_inputBox']")
+    public WebElement ButtonEmail;
+
+
+    @FindBy(xpath = "//button[@data-test-id='Forgot_password_sendEmailButton']")
+    public WebElement ButtonSendEmail;
+
+
+    @FindBy(xpath = "//div[@class='ant-message-custom-content ant-message-success']")
+    public WebElement Alert;
+
+    @FindBy(xpath = "//button[@id='accept']")
+    public WebElement MailCookies;
+
+    @FindBy(xpath = "//input[@placeholder='Saisissez une adresse ici']")
+    public WebElement MailSaissirAdresse;
+    @FindBy(xpath = "//i[@class='material-icons-outlined f36']")
+    public WebElement MailSaissirAdresseClick;
+    @FindBy(xpath = "//button[@id='refresh']")
+    public WebElement MailSaissirRefresh;
+
+    @FindBy(xpath = "//div[@class='bname']")
+    public WebElement YopmailMailAdress;
+
+    @FindBy(xpath = "(//p[@class='RegisterForm_differentAccount__riHel'])[1]")
+    public WebElement VerifyMailmesajVerificationforYopmail;
+
+
+    @FindBy(xpath = "//iframe[@id='ifmail']")
+    public WebElement IframeYopmail;
+
+    @FindBy(xpath = "//a[contains(text(),'PASSWORD RESET')]")
+    public WebElement PasswordResetButton;
+
+
+
+
+    @FindBy(xpath = "//input[@data-test-id=' newPassword_yourMailAddress']")
+    public WebElement EmailAddressForNewPassword;
+
+
+    @FindBy(xpath = "//input[@data-test-id='newPassword_newPassword']")
+    public WebElement NewPasswordforRESET;
+
+    @FindBy(xpath = "//input[@data-test-id='newwPassword_confirmPassword']")
+    public WebElement ComfirmPassword;
+
+    @FindBy(xpath = "//button[@data-test-id='newPassword_submitButton']")
+    public WebElement Submitbutton;
+
+
+
+    @FindBy(xpath = "//p[contains(text(),'Password has been updated')]")
+    public WebElement MessagePasswordHasBeenUpdated;
+
+    @FindBy(xpath = "//span[contains(text(),'Go to the Login Page')]")
+    public WebElement ButtonGoToLoginPage;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //FATIH YILMAZ UST
+
+
+
+
+
+
+
+
+
     @FindBy(xpath = "//input[@name='username']")
     public WebElement email_login;
     @FindBy(xpath = "//input[@name='password']")
