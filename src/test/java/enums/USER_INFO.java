9dad82bbf6e5742f39f46c594d418e17dfad3b8b
mervecakir <m.cakir88w@gmail.com>
2023-11-02 16:03:28 +0000

    US_044

--- src/test/java/enums/USER_INFO.java
+++ src/test/java/enums/USER_INFO.java
@@ -2,33 +2,6 @@ package enums;
 
 public enum USER_INFO {
 
-    THERAPIST("Merve","ybm88@yopmail.com","M1234&yb")
-    ;
 
-    private String name;
-    private String email;
-    private String password;
-
-    USER_INFO(String name, String email,String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void login(){
-        System.out.println(this.email +" " + this.name);
-    }
 }
 
