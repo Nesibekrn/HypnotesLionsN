266836885c9cb5215ba21a35e7666e059a098a42
zeynepsteel <lilyfoster1987@gmail.com>
2023-11-02 22:32:19 -0400

    us_79 is completed

--- src/test/java/pages/ClientsPage.java
+++ src/test/java/pages/ClientsPage.java
@@ -8,4 +8,24 @@ public class ClientsPage extends CommonPage{
     @FindBy(xpath = "//a[@href='/dashboard/clients']")
     public WebElement clients_button;
 
+    @FindBy(xpath = "//span[text()='Import Client']")
+    public WebElement importClientsButton;
+
+    @FindBy(xpath = "//div[text()='Choose Import Method']")
+    public WebElement chooseImportMethodWindow;
+
+    @FindBy(xpath = "//body/div[2]/div[1]/div[2]/div[1]/div[2]/div[2]/div[1]/button[4]")
+    public WebElement easyPracticeMethod;
+
+    @FindBy(xpath = "//input[@class=\"custom-file-input\"]")
+    public WebElement chooseFileForEasyPractice;
+
+    @FindBy(xpath = "//span[text()='Upload']")
+    public WebElement uploadButtonForEasyPractice;
+
+
+    @FindBy(xpath = "//h5[contains(text(),\"Zeynep butterfly\")]")
+    public WebElement importedClient;
+
+
 }
---
+++ src/test/java/stepDefinitions/UI_StepDef/clients/US_079.java
@@ -0,0 +1,62 @@
+package stepDefinitions.UI_StepDef.clients;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.CommonPage;
+
+import java.io.File;
+
+import static stepDefinitions.Hooks.driver;
+import static utilities.ReusableMethods.waitFor;
+
+public class US_079 extends CommonPage {
+
+    @Given("the user clicks on client title on the left menu")
+    public void the_user_clicks_on_client_title_on_the_left_menu() {
+        getClientsPage().clients_button.click();
+    }
+    @Then("Import Client button is displayed")
+    public void import_client_button_is_displayed() {
+        System.out.println(getClientsPage().importClientsButton.getText());
+        Assert.assertTrue(getClientsPage().importClientsButton.getText().contains("Import Client"));
+    }
+    @Then("the user is able to open to the relavant page once clicked on the import client button")
+    public void the_user_is_able_to_open_to_the_relavant_page_once_clicked_on_the_import_client_button() {
+getClientsPage().importClientsButton.click();
+waitFor(4);
+        System.out.println(getClientsPage().chooseImportMethodWindow.getText());
+
+Assert.assertTrue(getClientsPage().chooseImportMethodWindow.getText().contains("Choose Import Method"));
+    }
+
+
+    @Given("the user clicks on the import client button")
+    public void the_user_clicks_on_the_import_client_button() {
+        getClientsPage().importClientsButton.click();
+    }
+    @Given("the user clicks Easy Practice on the choose import method window")
+    public void the_user_clicks_easy_practice_on_the_choose_import_method_window() {
+        getClientsPage().easyPracticeMethod.click();
+        waitFor(5);
+    }
+    @Given("the user selects a csv file")
+    public void the_user_selects_a_csv_file() {
+        File logoPath=new File("src/test/resources/Upload Images/clients.csv");
+        String path = logoPath.getAbsolutePath();
+        getClientsPage().chooseFileForEasyPractice.sendKeys(path);
+        waitFor(5);
+    }
+    @Then("the user clicks on the upload button")
+    public void the_user_clicks_on_the_upload_button() {
+      getClientsPage().uploadButtonForEasyPractice.click();
+      waitFor(5);
+      driver.navigate().refresh();
+    }
+    @Then("user is able to view imported clients on the page")
+    public void user_is_able_to_view_imported_clients_on_the_page() {
+       String clientName="Zeynep butterfly";
+       waitFor(5);
+        Assert.assertEquals(clientName,getClientsPage().importedClient.getText());
+    }
+}
