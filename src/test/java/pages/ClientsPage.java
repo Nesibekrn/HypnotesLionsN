1108d1e40414fe271462959daf0ae40f26273e09
fatmacekimguler <fatmacekimguler@gmail.com>
2023-11-03 00:35:20 +0000

    US_050 locators is done

--- src/test/java/pages/ClientsPage.java
+++ src/test/java/pages/ClientsPage.java
@@ -7,5 +7,42 @@ public class ClientsPage extends CommonPage{
 
     @FindBy(xpath = "//a[@href='/dashboard/clients']")
     public WebElement clients_button;
-
+    @FindBy(xpath = "//a[@href='/dashboard/add-client']")
+    public WebElement addNewClient_button;
+    @FindBy(xpath = "//input[@id='register_firstName']")
+    public WebElement firstName_input;
+    @FindBy(xpath = "//input[@id='register_middleName']")
+    public WebElement middleName_input;
+    @FindBy(xpath = "//input[@id='register_lastName']")
+    public WebElement lastName_input;
+    @FindBy(xpath = "//input[@id='register_email']")
+    public WebElement email_input;
+    @FindBy(xpath = "//input[@id='register_gender']")
+    public WebElement gender_dropDown;
+    @FindBy(css = "div.selected-flag")
+    public WebElement phoneCode_dropDown;
+    @FindBy(css = "div.form-control false")
+    public WebElement phone_input;
+    @FindBy(xpath = "//input[@id='register_occupation']")
+    public WebElement occupation_input;
+    @FindBy(xpath = "//input[@id='register_zipCode']")
+    public WebElement zipCode_input;
+    @FindBy(xpath = "//input[@name='country']")
+    public WebElement country_input;
+    @FindBy(xpath = "//input[@name='state']")
+    public WebElement state_input;
+    @FindBy(xpath = "//input[@name='city']")
+    public WebElement city_input;
+    @FindBy(xpath = "//input[@id='register_address']")
+    public WebElement address_input;
+    @FindBy(xpath = "//input[@id='register_timezone']")
+    public WebElement timezone_dropDown;
+    @FindBy(xpath = "//input[@type='file']")
+    public WebElement uploadProfileImage_input;
+    @FindBy(xpath = "//button[@class='ant-btn css-aqx16b ant-btn-primary']")
+    public WebElement save_button;
+    @FindBy(xpath = "//a[@class='ant-btn css-aqx16b ant-btn-default ant-btn-dangerous']")
+    public WebElement cancel_button;
+    @FindBy(xpath = "//input[@class='ant-checkbox-input']")
+    public WebElement showExtraFields_checkbox;
 }
