db1b720e692809590cf0cfb2e03fd384211d79b9
mervecakir <m.cakir88w@gmail.com>
2023-11-06 18:45:40 +0000

    US42 and US55

--- src/test/java/stepDefinitions/UI_StepDef/calendar/US_042.java
+++ src/test/java/stepDefinitions/UI_StepDef/calendar/US_042.java
@@ -13,24 +13,27 @@ import static stepDefinitions.Hooks.driver;
 public class US_042 extends CommonPage {
     @Given("user login as therapist")
     public void userLoginAsTherapist() {
-   
 
     }
 
     @And("the user verifies that Calendar button is clickable")
     public void theUserVerifiesThatCalendarButtonIsClickable() {
-        Assert.assertTrue(getCalendarPage().calendar_buton.isEnabled());
+    Assert.assertTrue(getCalendarPage().calendar_buton.isEnabled());
+    ReusableMethods.waitFor(2);
 
     }
 
     @And("the user clicks on Calendar button")
     public void theUserClicksOnCalendarButton() {
         getCalendarPage().calendar_buton.click();
+        ReusableMethods.waitFor(2);
     }
 
     @Then("the user verifies Calendar page")
     public void theUserVerifiesCalendarPage() {
         Assert.assertEquals( "https://test.hypnotes.net/dashboard/calendar",driver.getCurrentUrl());
     }
+
+
 
 }
