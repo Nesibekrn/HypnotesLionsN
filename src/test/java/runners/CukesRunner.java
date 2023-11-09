c3758792d04daaf84c057c82ac54db2f87c2c336
Serpil-Dndr <serpil.dndrca@gmail.com>
2023-11-08 21:45:21 -0500

    update

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "stepDefinitions",
-        tags = "@US_053",
+        tags = "",
 
         dryRun = false
 )
