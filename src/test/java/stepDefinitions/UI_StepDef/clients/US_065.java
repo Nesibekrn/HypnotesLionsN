8034a2df502a942e3f47b90a9cab070bda0e9610
Rue Ertekin <ertekin.rue@gmail.com>
2023-11-09 10:56:15 -0800

    Update step definitions for US_065 and US_074

--- src/test/java/stepDefinitions/UI_StepDef/clients/US_065.java
+++ src/test/java/stepDefinitions/UI_StepDef/clients/US_065.java
@@ -4,7 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
-public class US_065_StepDef {
+public class US_065 {
     @Given("As a Therapist user clicks on Appointments")
     public void asATherapistUserClicksOnAppointments() {
     }
