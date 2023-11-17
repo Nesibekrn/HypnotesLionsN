6c2dc923fba8089bffa37ab9a3b2c8a459f7f292
fatmacekimguler <fatmacekimguler@gmail.com>
2023-11-17 01:17:29 +0000

    US218 is done

--- src/test/java/stepDefinitions/API_StepDef/US_218.java
+++ src/test/java/stepDefinitions/API_StepDef/US_218.java
@@ -20,6 +20,8 @@ public class US_218 {
     Response response;
     int id;
     JsonPath jsonPath;
+    String expectStartAt;
+    String expectFinishAt;
 
     @When("user sends Post request to update block times")
     public void userSendsPostRequestToUpdateBlockTimes() {
@@ -28,14 +30,15 @@ public class US_218 {
         //specFormData.pathParams("first", "api", "second", "hypnotherapist", "third", "timeoff", "fourth", "all");
         specFormData.pathParams("first", "api", "second", "hypnotherapist", "third", "timeoff", "fourth", "update");
         // Set the expected Data
-
+        expectStartAt = "2023-11-17 20:30";
+        expectFinishAt = "2023-11-17 21:00";
         // response = given(specFormData).when().post("{first}/{second}/{third}/{fourth}");
         Map<String, Object> payloadUpdate = new HashMap<>();
-        payloadUpdate.put("id", id);
-        payloadUpdate.put("specificDate", "2023-11-19");
+        payloadUpdate.put("id", 1411);
+        payloadUpdate.put("specificDate", "2023-11-17");
         payloadUpdate.put("isRecurring", false);
-        payloadUpdate.put("startAt", "2023-11-19 17:30");
-        payloadUpdate.put("finishAt", "2023-11-19 18:00");
+        payloadUpdate.put("startAt", expectStartAt);
+        payloadUpdate.put("finishAt", expectFinishAt);
         payloadUpdate.put("isAllDay", false);
 
         // response = given(specFormData).formParams(payloadCreate).post("{first}/{second}/{third}/{fourth}");
@@ -46,6 +49,7 @@ public class US_218 {
         response.prettyPrint();
         /*id = response.jsonPath().getInt("data[0].id");
         System.out.println("id = " + id);*/
+        Assert.assertEquals(expectStartAt,response.jsonPath().get("data[0].startAt").toString());
     }
 
     @Then("user validates the response")
@@ -55,10 +59,12 @@ public class US_218 {
 
     @When("user sends Post request to add block times")
     public void userSendsPostRequestToAddBlockTimes() {
+        expectStartAt = "2023-11-18 10:30";
+        expectFinishAt = "2023-11-18 11:00";
         Map<String, Object> payloadCreate = new HashMap<>();
         payloadCreate.put("specificDate", "2023-11-18");
-        payloadCreate.put("startAt", "2023-11-18 12:00");
-        payloadCreate.put("finishAt", "2023-11-18 12:30");
+        payloadCreate.put("startAt", expectStartAt);
+        payloadCreate.put("finishAt", expectFinishAt);
         payloadCreate.put("isRecurring", false);
         payloadCreate.put("isAllDay", false);
         payloadCreate.put("title", "Online");
@@ -69,24 +75,26 @@ public class US_218 {
         id = jsonPath.getInt("data[0].id");
         System.out.println("id = " + id);
         response.prettyPrint();
+        Assert.assertEquals(expectStartAt,response.jsonPath().get("data[0].startAt").toString());
+
     }
 
     @When("user sends Post request to delete block times")
     public void userSendsPostRequestToDeleteBlockTimes() {
         Map<String, Object> payloadDelete = new HashMap<>();
-        payloadDelete.put("id", id);
-        response=given().header("Cookie",phpSessid).formParams(payloadDelete).post("https://test.hypnotes.net/api/hypnotherapist/timeoff/delete");
+        payloadDelete.put("id", 1412);
+        response = given().header("Cookie", phpSessid).formParams(payloadDelete).post("https://test.hypnotes.net/api/hypnotherapist/timeoff/delete");
         response.prettyPrint();
     }
 
     @When("user sends Post request to all block times")
     public void userSendsPostRequestToAllBlockTimes() {
         Map<String, Object> payloadAll = new HashMap<>();
-        payloadAll.put("title","Online");
+        payloadAll.put("title", "Online");
 
         specFormData.pathParams("first", "api", "second", "hypnotherapist", "third", "timeoff", "fourth", "all");
 
-        response=given(specFormData).when().post("{first}/{second}/{third}/{fourth}");
+        response = given(specFormData).when().post("{first}/{second}/{third}/{fourth}");
         response.prettyPrint();
     }
 }
