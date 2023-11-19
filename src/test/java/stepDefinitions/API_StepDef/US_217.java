2f41ece78aedc8a4b2b12c628a52d0726ba1fdde
zeynepsteel <lilyfoster1987@gmail.com>
2023-11-18 21:53:12 -0500

    217 api

--- src/test/java/stepDefinitions/API_StepDef/US_217.java
+++ src/test/java/stepDefinitions/API_StepDef/US_217.java
@@ -5,7 +5,11 @@ import io.cucumber.java.en.*;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
+import pojos.Datum;
+import pojos.Root;
 
+
+import java.util.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,8 +20,11 @@ import static io.restassured.RestAssured.given;
 public class US_217 {
 
     Response response;
-    Integer idCreated,idSon;
     JsonPath jsonPath;
+
+    Datum data1, data2;
+
+    Root expectedData;
 
 
     @When("user sends POST request to get all blocked times")
@@ -31,13 +38,45 @@ public class US_217 {
         response = given(specFormData).formParams(payloadCreate).post("{p1}/{p2}/{p3}/{p4}");
         jsonPath = response.jsonPath();
         response.prettyPrint();
+        //second way with POJO class
+//  // set expected data
+//        ArrayList<Object> recurringDays=new ArrayList<>();
+//        ArrayList<Datum> arrayOfData=new ArrayList<>();
+//data1=new Datum(1472,false,"2023-11-18 15:40","2023-11-18 15:45",recurringDays,"2023-11-18T00:00:00+00:00",false,"Online");
+//expectedData=new Root(true,arrayOfData );
+//        response = given(specFormData).formParams(payloadCreate).post("{p1}/{p2}/{p3}/{p4}");
+//        response.prettyPrint();
+
+        jsonPath = response.jsonPath();
+        Map<Object, Object> responseBody = response.as(Map.class);
+        responseBody.entrySet().stream().forEach(t-> System.out.println("****"+t.getValue())
+
+        );
+
+        }
 
 
+
+    @Then("user see {int} status code and response body contains status as true")
+    public void user_see_status_code_and_response_body_contains_status_as_true(int statusCode) {
+      Assert.assertEquals(statusCode,response.getStatusCode());
+        System.out.println(jsonPath.getBoolean("status"));
+      Assert.assertTrue(jsonPath.getBoolean("status"));
+
+      //      Assert.assertEquals(expectedData.status,jsonPath.getBoolean("status"));
     }
-    @Then("user see {int} status code")
-    public void user_see_status_code(int int1) {
-      Assert.assertEquals(int1,response.getStatusCode());
+
+
+    @Then("the response body contains id as {string}")
+    public void the_response_body_contains_id_as(String id) {
+      String ExpectedId= response.path("data[0].id").toString();
+        Assert.assertEquals(id, ExpectedId);
     }
 
+    @Then("the response returns empty array")
+    public void the_response_returns_empty_array() {
+      //  System.out.println("size"+response.path("data[].size"));
+        //Assert.assertTrue(response.path("data[].size"));
+    }
 
 }
--- src/test/resources/features/API_features/US_217.feature
+++ src/test/resources/features/API_features/US_217.feature
@@ -1,5 +1,13 @@
 @API @US_217
-Feature: Hypnotes
+Feature: Hypnotes-API functionality
   Scenario: As a user, I should be able to get all block times with using API endpoints
-When user sends POST request to get all blocked times
-    Then user see 200 status code
+    When user sends POST request to get all blocked times
+    Then user see 200 status code and response body contains status as true
+
+  Scenario: Verify that the user able to valid id on the response
+    When user sends POST request to get all blocked times
+    Then the response body contains id as "1481"
+
+    Scenario: Verify that when the user has no blocked time the response body return with an empty data array list
+      When user sends POST request to get all blocked times
+      Then the response returns empty array
