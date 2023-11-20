2563a4141c94019666c88b1f22e81d203d79dadb
DuyguTo <duygutozanca@gmail.com>
2023-11-20 10:50:38 -0500

    Api

---
+++ src/test/java/stepDefinitions/API_StepDef/US_204_API.java
@@ -0,0 +1,39 @@
+package stepDefinitions.API_StepDef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import stepDefinitions.Hooks;
+import utilities.API_utilities;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class US_204_API {
+    Response response;
+    Map<String,Object> payload=new HashMap<>();
+
+    List<Integer> id=new ArrayList<>();
+
+    @Given("the user sends a request to get all meeting info on the Hypnotes calendar page")
+    public void theUserSendsARequestToGetAllMeetingInfoOnTheHypnotesCalendarPage() {
+        API_utilities.login("threapistlions@yopmail.com","Test123.");
+       response=given().post("https://test.hypnotes.net/api/settings/meeting/get");
+       response.prettyPrint();
+    }
+
+    @Then("user verifies that status code is {int}")
+    public void userVerifiesThatStatusCodeIs(int statusCode) {
+        Assert.assertEquals(statusCode,response.getStatusCode());
+    }
+
+    @And("the user verifies id of client")
+    public void theUserVerifiesIdOfClient() {
+    }
+}
