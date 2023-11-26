50761b6e5ae4d09e63e22a0b8241c445d345ef33
fatmacekimguler <fatmacekimguler@gmail.com>
2023-11-26 16:41:04 +0000

    US093

--- src/test/java/stepDefinitions/UI_StepDef/clients/US_093.java
+++ src/test/java/stepDefinitions/UI_StepDef/clients/US_093.java
@@ -1,4 +1,24 @@
 package stepDefinitions.UI_StepDef.clients;
 
-public class US_093 {
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import pages.CommonPage;
+
+public class US_093 extends CommonPage {
+    @And("user clicks Schedule an Appointment at the selected client")
+    public void userClicksScheduleAnAppointmentAtTheSelectedClient() {
+      getClientsPage().button_ScheduleAnAppointment.click();
+    }
+
+    @When("user selects {string} at the Service section")
+    public void userSelectsAtTheServiceSection(String service) {
+        getClientsPage().service_inputSelect2.sendKeys(service);
+    }
+
+    @And("user selects Online at the Locations section")
+    public void userSelectsOnlineAtTheLocationsSection() {
+        getClientsPage().dropDown_ScheduleAnAppointmentLocationsSelect.click();
+    }
+
+
 }
