76672635a86d120b9912bfb8f897371b7c5e5f90
fatmacekimguler <fatmacekimguler@gmail.com>
2023-11-28 23:31:18 +0000

    US235

--- src/test/java/stepDefinitions/API_StepDef/US_235.java
+++ src/test/java/stepDefinitions/API_StepDef/US_235.java
@@ -5,14 +5,6 @@ import io.cucumber.java.en.*;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Assert;
-import utilities.API_utilities;
-import utilities.ReusableMethods;
-
-import javax.swing.text.Utilities;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,9 +17,10 @@ public class US_235 {
     Response response;
     JsonPath jsonPath;
     private String title = Faker.instance().ancient().titan();
+    private String newTitle = Faker.instance().ancient().titan();
     private String price = "100";
     private String duration = "60";
-    private int attendeeLimit=Faker.instance().number().numberBetween(3, 8);
+    private int attendeeLimit=Faker.instance().number().numberBetween(2, 8);
     
     private String dateF;
     private String timeF ;
@@ -39,15 +32,16 @@ public class US_235 {
         specFormDataGroupSession.pathParams("p1", "api", "p2", "settings", "p3", "meeting", "p4", "category", "p5", "add");
         dateF = nextDate(2);
         timeF = nextTime(2);
-        String attendeeLimitReverse = Integer.toString(attendeeLimit);
-        System.out.println(" attendeeLimit = " +  attendeeLimitReverse);
+       // String attendeeLimitReverse = Integer.toString(attendeeLimit);
+      //  System.out.println(" attendeeLimit = " +  attendeeLimitReverse);
         payload.put("title", title);
         payload.put("price", price);
         payload.put("duration", duration);
-        payload.put("attendeeLimit", attendeeLimitReverse);
+        payload.put("attendeeLimit", attendeeLimit);
         payload.put("sessionDate", dateF);
         payload.put("sessionTime", timeF);
-
+        System.out.println("dateF = " + dateF);
+        System.out.println("timeF = " + timeF);
         response = given(specFormDataGroupSession).formParams(payload).post("{p1}/{p2}/{p3}/{p4}/{p5}");
         jsonPath = response.jsonPath();
         groupSessionId = jsonPath.get("category.id");
@@ -66,12 +60,25 @@ public class US_235 {
 
     @Then("user sends Post request to update group session")
     public void user_sends_post_request_to_update_group_session() {
-
+        specFormDataGroupSession.pathParams("p1", "api", "p2", "settings", "p3", "meeting", "p4", "category", "p5", "update");
+        payload.put("title", newTitle);
+        payload.put("price", price);
+        payload.put("duration", duration);
+        payload.put("attendeeLimit", attendeeLimit);
+        payload.put("sessionDate", dateF);
+        payload.put("sessionTime", timeF);
+        payload.put("categoryId",groupSessionId);
+        response=given(specFormDataGroupSession).formParams(payload).post("{p1}/{p2}/{p3}/{p4}/{p5}");
+        jsonPath=response.jsonPath();
+        response.prettyPrint();
     }
 
     @Then("user validates the response for update group session")
     public void user_validates_the_response_for_update_group_session() {
-
+        Assert.assertTrue(jsonPath.getBoolean("success"));
+        Assert.assertEquals(newTitle, jsonPath.getString("category.title"));
+        Assert.assertEquals(price, jsonPath.getString("category.price"));
+        Assert.assertEquals(duration, jsonPath.getString("category.duration"));
     }
 
     @Then("user sends Post request to delete group session")
