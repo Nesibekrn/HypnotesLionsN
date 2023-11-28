d9bd62bde6d7e175b9d3ec0c3c307e4bd6728975
fatmacekimguler <fatmacekimguler@gmail.com>
2023-11-28 18:07:34 +0000

    US_235

--- src/test/java/stepDefinitions/API_StepDef/US_235.java
+++ src/test/java/stepDefinitions/API_StepDef/US_235.java
@@ -1,5 +1,6 @@
 package stepDefinitions.API_StepDef;
 
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.*;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
@@ -12,12 +13,23 @@ import static base_url.HypnotesBaseUrl.specFormDataGroupSession;
 public class US_235 {
     Response response;
     JsonPath jsonPath;
+    private String title = Faker.instance().ancient().titan();
+    private int price = 100;
+    private int duration = 60;
+    private int attendeeLimit = Faker.instance().number().numberBetween(3, 8);
+    private String date = "";
+    private String time = "";
 
     @When("user sends Post request to add group session")
     public void user_sends_post_request_to_add_group_session() {
         specFormDataGroupSession.formParams("p1", "api", "p2", "settings", "p3", "meeting", "p4", "category", "p5", "add");
         Map<String, Object> payload = new HashMap<>();
         payload.put("title", "New Group Session");
+        payload.put("price", price);
+        payload.put("duration", duration);
+        payload.put("attendeeLimit", attendeeLimit);
+        payload.put("sessionDate", date);
+        payload.put("sessionTime",time);
     }
 
     @Then("user validates the response for add group session")
