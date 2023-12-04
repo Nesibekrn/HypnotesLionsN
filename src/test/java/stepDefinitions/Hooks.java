0f40d8596ad4ce84f9f2f4f1567d44bbf741ae72
fatmacekimguler <fatmacekimguler@gmail.com>
2023-12-04 22:16:48 +0000

    Conflict is resolved

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -16,7 +16,8 @@ import utilities.Driver;
 import utilities.ReusableMethods;
 
 
-import static base_url.HypnotesBaseUrl.*;
+import static base_url.HypnotesBaseUrl.hypnotesSetUpFormData;
+import static base_url.HypnotesBaseUrl.hypnotesSetUpFormDataForGroupSession;
 import static base_url.baseUrl_fy.hypnotesSetUp1;
 
 import static base_url.baseUrl_fy.hypnotesSetUp1;
