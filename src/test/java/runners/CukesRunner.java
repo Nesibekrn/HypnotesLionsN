f2180481d5453b8377bdc9865327c0ebb9fb1f40
fatmacekimguler <fatmacekimguler@gmail.com>
2023-12-04 22:03:28 +0000

    US235 is done

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
        glue = "stepDefinitions",
         dryRun = false,
-        tags = "@US235"
+        tags = ""
 
 )
 public class CukesRunner {
