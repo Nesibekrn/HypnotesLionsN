6eb2adb2b925fc5ec194eb6461186cd4952bec9a
mervecakir <m.cakir88w@gmail.com>
2023-12-07 19:21:04 +0000

    US-238 is updated

--- src/test/java/stepDefinitions/API_StepDef/US_238.java
+++ src/test/java/stepDefinitions/API_StepDef/US_238.java
@@ -1,8 +1,7 @@
 package stepDefinitions.API_StepDef;
 
 import com.github.javafaker.Faker;
-import enums.Enum_Fy;
-import enums.USER_INFO;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -13,31 +12,30 @@ import static utilities.API_utilities.*;
 
 public class US_238 {
 
-
-    String firstName = Faker.instance().name().firstName();
-    String newFirstName = Faker.instance().name().firstName();
-    String lastName = Faker.instance().name().lastName();
-    String newLastName = Faker.instance().name().lastName();
-    String email = Faker.instance().internet().emailAddress();
+    Faker faker=new Faker();
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String email = faker.internet().emailAddress();
+   // String firstName2= faker.name().firstName();
+   //String lastName2 = faker.name().lastName();
     String timeZone = "Europe/London";
-
 
     int clientID;
 
 
     @Given("the user request to add a new client from api")
     public void theUserRequestToAddANewClientFromApi() {
-        payload.put("firstName",firstName);
-        payload.put("lastName",lastName);
-        payload.put("email",email);
-        payload.put("timeZone",timeZone);
+        payload.put("firstName", firstName);
+        payload.put("lastName", lastName);
+        payload.put("email", email);
+        payload.put("timeZone", timeZone);
 
         response = given()
-                .header("cookie",  "PHPSESSID=" + phpSessId)
+                .header("cookie", "PHPSESSID=" + phpSessId)
                 .formParams(payload)
                 .post("/dashboard/client/add");
         response.prettyPrint();
-        clientID=response.jsonPath().getInt("client.id");
+        clientID = response.jsonPath().getInt("client.id");
         System.out.println("clientID = " + clientID);
 
 
@@ -46,28 +44,32 @@ public class US_238 {
     @And("the user validates the response for add new client")
     public void theUserValidatesTheResponseForAddNewClient() {
         Assert.assertTrue(response.jsonPath().getBoolean("success"));
-        Assert.assertEquals(firstName,response.jsonPath().getString("client.name"));
-        Assert.assertEquals(clientID,response.jsonPath().getInt("client.id"));
+        Assert.assertEquals(firstName, response.jsonPath().getString("client.name"));
+        Assert.assertEquals(clientID, response.jsonPath().getInt("client.id"));
 
     }
 
+
     @Then("the user request to update the client from api")
     public void theUserRequestToUpdateTheClientFromApi() {
-        payload.put("firstName",newFirstName);
-        payload.put("lastName",newLastName);
-        payload.put("country","England");
-        payload.put("gender","Female");
-        payload.put("clientId",clientID);
+        payload.put("name","ALI");
+        payload.put("surname", "VELI");
+        payload.put("email",email);
+        payload.put("country", "England");
+        payload.put("gender", "Female");
+        payload.put("clientId", clientID);
+        payload.put("timeZone", timeZone);
         response = given()
-                .header("cookie",  "PHPSESSID=" + phpSessId)
+                .header("cookie", "PHPSESSID=" + phpSessId)
                 .formParams(payload)
                 .post("/dashboard/client/update");
         response.prettyPrint();
 
-    }
 
+    }
     @Then("the user verifies client is updated")
     public void theUserVerifiesClientIsUpdated() {
         Assert.assertTrue(response.jsonPath().getBoolean("success"));
     }
+
 }
