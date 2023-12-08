7e0eb535ddd16c7bebd7529d257d6e0a4620caa4
yasemin <ymacit0955@gmail.com>
2023-12-08 15:56:44 +0000

    US_224 is done.

--- src/test/java/stepDefinitions/API_StepDef/US_224.java
+++ src/test/java/stepDefinitions/API_StepDef/US_224.java
@@ -2,10 +2,13 @@ package stepDefinitions.API_StepDef;
 
 import io.cucumber.java.en.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static stepDefinitions.Hooks.response;
 import static utilities.API_utilities.phpSessId;
@@ -13,9 +16,10 @@ import static utilities.API_utilities.phpSessId;
 public class US_224 {
     Map<String, Object> payload=new HashMap<>();
     int createdIndvSessionId;
+    List<Integer> allIndividualSessIds=new ArrayList<>();
     @When("user sends a post request to add a new individual session")
     public void user_sends_a_post_request_to_add_a_new_individual_session() {
-        payload.put("title", "Depression4");
+        payload.put("title", "Depression7");
         payload.put("meetingType","standartMeeting");
         payload.put("price",50);
         payload.put("duration",60);
@@ -33,6 +37,13 @@ public class US_224 {
     }
     @Then("user verifies the title,meeting type,duration")
     public void user_verifies_the_title_meeting_type_duration() {
+        assertEquals(payload.get("title"),response.jsonPath().getString("category.title"));
+        assertEquals(payload.get("meetingType"),response.jsonPath().getString("category.meetingType"));
+        assertEquals(payload.get("price"),response.jsonPath().getInt("category.price"));
+        assertEquals(payload.get("duration"),response.jsonPath().getInt("category.duration"));
+        assertEquals(payload.get("blockBefore"),response.jsonPath().getInt("category.blockBefore"));
+        assertEquals(payload.get("blockAfter"),response.jsonPath().getInt("category.blockAfter"));
+
 
 
     }
@@ -43,10 +54,14 @@ public class US_224 {
                 .post("https://test.hypnotes.net/api/settings/meeting/category/get");
         response.prettyPrint();
 
+        allIndividualSessIds=response.jsonPath().get("id");
+        System.out.println("allIndividualSessIds = " + allIndividualSessIds);
+
 
     }
     @Then("user should see the added session in this list")
     public void user_should_see_the_added_session_in_this_list() {
+        assertTrue(allIndividualSessIds.contains(createdIndvSessionId));
 
     }
     @When("user sends the request to delete added session")
