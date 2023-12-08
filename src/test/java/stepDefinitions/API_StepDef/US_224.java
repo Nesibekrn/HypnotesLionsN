78a70761ccb0314edbead574b6423e883f867967
yasemin <ymacit0955@gmail.com>
2023-12-08 06:24:27 +0000

    Us_224 is in progress

---
+++ src/test/java/stepDefinitions/API_StepDef/US_224.java
@@ -0,0 +1,36 @@
+package stepDefinitions.API_StepDef;
+
+import io.cucumber.java.en.*;
+
+public class US_224 {
+    @When("user sends a post request to add a new individual session")
+    public void user_sends_a_post_request_to_add_a_new_individual_session() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user verifies the title,meeting type,duration")
+    public void user_verifies_the_title_meeting_type_duration() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user sends the request to get all individiual sessions")
+    public void user_sends_the_request_to_get_all_individiual_sessions() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see the added session in this list")
+    public void user_should_see_the_added_session_in_this_list() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user sends the request to delete added session")
+    public void user_sends_the_request_to_delete_added_session() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("added session should be deleted successfully.")
+    public void added_session_should_be_deleted_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
